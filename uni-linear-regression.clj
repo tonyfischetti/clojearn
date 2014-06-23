f9a617a8354d9832b4e9ffafe5566fcce5ab7ac5
Tony Fischetti <tony.fischetti@gmail.com>
2014-06-23 18:48:32 -0400

    added standard deviation

--- uni-linear-regression.clj
+++ uni-linear-regression.clj
@@ -19,18 +19,9 @@
   (map #(Math/pow % 2)
        (map - x-vec y-vec)))
 
-; computes the mean sum-of-squared residual
-(defn mean-sum-squared-residuals [residuals]
-  (/ (reduce + residuals)
-     (count residuals)))
-
-;   COST FUNCTION
-; this uses 0.5 times the mean sum-of-squared
-; residuals to make the partial derivatives
-; in the gradient descent algorithm easier
-(defn cost-function [x-vec y-vec]
-  (* 0.5 (mean-sum-squared-residuals
-         (residuals x-vec y-vec))))
+; returns the mean of a vector
+(defn mean [a-vector]
+  (/ (reduce + a-vector) (count a-vector)))
 
 ; returns highest value minus value
 ; in vector
@@ -38,9 +29,14 @@
   (- (apply max a-vector)
      (apply min a-vector)))
 
-; returns the mean of a vector
-(defn mean [a-vector]
-  (/ (reduce + a-vector) (count a-vector)))
+
+; computes sample standard deviation
+(defn st-dev [a-vector]
+  (let [themean (mean a-vector)]
+    (Math/pow (/ (reduce + (map #(Math/pow % 2)
+                             (map #(- % themean) a-vector)))
+                 (- (count a-vector) 1)) 0.5)))
+
 
 ; returns a transformed vector where each
 ; element is subtracted by 'centerer' and
@@ -52,16 +48,14 @@
   (map #(/ % scaler)
        (map #(- % centerer) a-vector)))
 
-; testing
-(let [test-vec [1 8 2 4 10 3 6]]
-  (println (scale test-vec 10 5))
-  (println (scale test-vec (vect-range test-vec)
-                           (mean test-vec)))
-  (println (mean test-vec)))
 
 
-(println (cost-function (make-prediction 0 0 [1 2 3]) [1 2 3]))
-
+;   COST FUNCTION
+; this uses 0.5 times the mean sum-of-squared
+; residuals to make the partial derivatives
+; in the gradient descent algorithm easier
+(defn cost-function [x-vec y-vec]
+  (* 0.5 (mean (residuals x-vec y-vec))))
 
 
 
@@ -70,3 +64,20 @@
   (println x-vec)
   (println y-vec))
 ; adaptive learning rate?
+
+
+
+
+; testing
+(let [test-vec [1 8 2 4 10 3 6]]
+  (println (st-dev test-vec))
+  (println (scale test-vec 10 5))
+  (println (scale test-vec (vect-range test-vec)
+                           (mean test-vec)))
+  (println (mean test-vec))
+  (println (cost-function (make-prediction 0 0 [1 2 3]) [1 2 3])))
+
+
+
+
+
